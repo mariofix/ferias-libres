9f7b21c4ffb6fd723c62f0983bc83260e7769048
Mario Hernandez <mariofix@proton.me>
2023-07-04 18:26:14 -0400

    Sentry y expo-doctor

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
+import * as Sentry from "sentry-expo";
+
+Sentry.init({
+  dsn: "https://7aad586aa4bf420892c1cccf4e97ec0b@o4505459108347904.ingest.sentry.io/4505459110182912",
+  enableInExpoDevelopment: true,
+  debug: true,
+});
+
 import "expo-router/entry";
